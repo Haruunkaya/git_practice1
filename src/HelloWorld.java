9c1acd657e82bd690ad694137561cb539f1e82ac
Harun <haruunseyma@gmail.com>
2022-05-04 04:43:45 +0300

    work 3 is done and about to be pushed to Github after commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,4 +3,8 @@ public class HelloWorld {
     //doing some work  here(buraya birşeyle yaz)
 
     //doing awesome work 2!
+
+    //doing awesome work 4!
+    //Awesome work 3 is done and ready to be committed
+    // and ready to be pushed to remote repository
 }
